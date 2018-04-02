7b4fb1090d7c36346cd2b2c42138061a9d1313f9
@asadsiddique! <asad.siddique712@gmail.com>
2018-04-02 18:25:23 +0500

    a

---
+++ labproject/src/labproject/mainclass.java
@@ -0,0 +1,15 @@
+package labproject;
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		shoppingcart ob1=new shoppingcart();
+		ob1.addItems("shirt");
+		ob1.addItems("pent");
+		ob1.addItems("belt");
+		ob1.addItems("shose");
+		ob1.getNumberOfItems();
+
+	}
+
+}
--- labproject/src/labproject/shoppingcart.java
+++ labproject/src/labproject/shoppingcart.java
@@ -1,7 +1,29 @@
 package labproject;
-
+import java.util.ArrayList;
 public class shoppingcart {
-	//tyghtryhbty
-	//yujyujyu
 
-}
+	 private static ArrayList<String> items;
+	    double total;
+
+	    public shoppingcart(){
+	        items = new ArrayList<String>();
+	    }
+
+	    public void addItems(String item){
+	        items.add(item);
+	    }
+
+	    public void removeItems(String item){
+	        items.remove(item);
+	    }
+
+	    public void getNumberOfItems(){
+	        System.out.println(items.size());
+	    }
+
+	    public String getItemName(int index){
+	        return items.get(index);
+	    }
+
+	
+	}
